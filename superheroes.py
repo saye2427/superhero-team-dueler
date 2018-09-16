67d29073e42e903df20a105223b97754d1994a89
Sayema Islam <sayema.islam@gmail.com>
2018-09-16 16:06:05 -0700

    completed ability class

--- superheroes.py
+++ superheroes.py
@@ -1,3 +1,5 @@
+import random
+
 class Ability:
     def _init_(self, name, attack_strength):
         # Initialize starting values
@@ -5,7 +7,12 @@ class Ability:
         self.attack_strength = attack_strength
     def attack(self):
         # Return attack value
-        return attack_strength
+        # Calculate the lowest attack value as an integer
+        self.lowest_attack_val = attack_strength // 2
+        # Use random.randint(a, b) to select a random attack value.
+        self.random_attack = random.randint(lowest_attack_val, attack_strength)
+        # Return attack value between 0 and the full attack.
+        return random_attack
     def update_attack(self, attack_strength):
         # Update attack value
         self.attack_strength = attack(self)
